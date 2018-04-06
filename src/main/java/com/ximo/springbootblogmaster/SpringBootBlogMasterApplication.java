a4db8bbe1605c91b3f97d23dc26242025bc6921f
X1mo <1392936858@qq.com>
2018-04-06 19:56:50 +0800

    init

---
+++ src/main/java/com/ximo/springbootblogmaster/SpringBootBlogMasterApplication.java
@@ -0,0 +1,12 @@
+package com.ximo.springbootblogmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogMasterApplication.class, args);
+	}
+}
